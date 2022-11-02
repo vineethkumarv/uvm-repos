af67d2f557b1561eece5b709de2736767bd2ad2b
vineethkumarv <vineethveepuri@mirafra.com>
2022-11-03 02:12:09 +0530

    done

--- UDphase/forwiki.sv
+++ UDphase/forwiki.sv
@@ -21,7 +21,7 @@ class u_phase extends uvm_topdown_phase;
 
   virtual function void exec_func(uvm_component comp,uvm_phase phase);
   //display("in the function");
-  `uvm_info(get_full_name(),$sformatf("in %s phase",phase.get_name()),UVM_MEDIUM);
+  `uvm_info(get_type_name(),$sformatf("in %s",phase.get_name()),UVM_MEDIUM);
   endfunction
 endclass
 
@@ -29,6 +29,7 @@ class test extends uvm_test;
   `uvm_component_utils(test)
   function new(string n="test",uvm_component p=null);
     super.new(n,p);
+  //uvm_domain::get_common_domain().add(u_phase::get(),null,uvm_build_phase::get(),null);
   endfunction
   
   virtual task run_phase(uvm_phase phase); 
@@ -39,6 +40,7 @@ class test extends uvm_test;
   //uvm_domain u=uvm_domain::get_common_domain();
   //uvm_phase up=u.find(uvm_build_phase::get());
   //u.add(u_phase::get(),null,up,null);
+  //uvm_domain::get_common_domain().add(u_phase::get(),null,uvm_build_phase::get(),null);
   uvm_domain::get_common_domain().add(u_phase::get(),null,uvm_build_phase::get(),null);
   `uvm_info(get_type_name(),$sformatf("in %s phase",phase.get_name()),UVM_LOW);
   endfunction
