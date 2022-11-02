1ef9994b8461469687a1e9f07509ecf162970b03
vineethkumarv <vineethveepuri@mirafra.com>
2022-11-02 21:29:01 +0530

    edited ne1

--- UDphase/ne1.sv
+++ UDphase/ne1.sv
@@ -1,4 +1,3 @@
-
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
@@ -8,8 +7,6 @@ class extended_comp extends uvm_component;
     super.new(name, parent);
   endfunction
 
-  virtual task training_phase(uvm_phase phase);
-endtask
 endclass
 
 class u_phase extends uvm_topdown_phase;
@@ -17,21 +14,31 @@ class u_phase extends uvm_topdown_phase;
   function new(string n="u_phase");
     super.new(n);
   endfunction
+  
+  virtual function string get_type_name();
+    return "user_defined_phase";
+  endfunction
+  
+  /*virtual function string get_inst();
+    return "user_defined_phase";
+  endfunction*/
   static function u_phase get();
   if (up==null) begin
     up=new();
   end
     return up;
   endfunction
-  virtual function void exec_func(uvm_component comp,uvm_phase phase); 
-  //$display("in the function");
-  `uvm_info(get_type_name(),$sformatf("in %s phase",phase.get_name()),UVM_LOW);
-endfunction
+  
+  virtual function void exec_func(uvm_component comp,uvm_phase phase);
+  //display("in the function");
+  `uvm_info(get_full_name(),$sformatf("in %s phase",phase.get_name()),UVM_LOW);
+ endfunction
+
 endclass
 
 class test extends uvm_test;
   `uvm_component_utils(test)
-  extended_comp ec;
+  //extended_comp ec;
   //uvm_phase schedule,s1;
   uvm_domain udomain;
   function new(string n="test",uvm_component p=null);
@@ -41,25 +48,26 @@ class test extends uvm_test;
   virtual task run_phase(uvm_phase phase); 
   //$display("in the function");
   `uvm_info(get_type_name(),$sformatf("in %s phase",phase.get_name()),UVM_LOW);
-endtask
-virtual function void build_phase(uvm_phase phase);
-//super.build_phase(phase);
-`uvm_info(get_type_name(),$sformatf("in %s phase",phase.get_name()),UVM_LOW);
-ec =extended_comp::type_id::create("ec",this);
-udomain=uvm_domain::get_common_domain();
-//schedule = udomain.find(uvm_build_phase::get());
-//s1 = udomain.find(uvm_connect_phase::get());
-//schedule.add(u_phase::get(), .after_phase(uvm_build_phase::get()), .before_phase(uvm_connect_phase::get()));
-udomain.add(u_phase::get(),null,uvm_build_phase::get(),uvm_connect_phase::get());
+  endtask
+ 
+  virtual function void build_phase(uvm_phase phase);
+ //super.build_phase(phase);
+ `uvm_info(get_type_name(),$sformatf("in %s phase",phase.get_name()),UVM_LOW);
+ //ec =extended_comp::type_id::create("ec",this);
+ udomain=uvm_domain::get_common_domain();
+ //schedule = udomain.find(uvm_build_phase::get());
+ //s1 = udomain.find(uvm_connect_phase::get());
+ //schedule.add(u_phase::get(), .after_phase(uvm_build_phase::get()), .before_phase(uvm_connect_phase::get()));
+ udomain.add(u_phase::get(),null,uvm_build_phase::get(),uvm_connect_phase::get());
  endfunction
  virtual function void connect_phase(uvm_phase phase);
-
  `uvm_info(get_type_name(),$sformatf("in %s phase",phase.get_name()),UVM_LOW);
-endfunction
+ endfunction
+
 endclass
 
-import uvm_pkg::*;
 module one;
+import uvm_pkg::*;
 
 initial begin
   run_test("test");
